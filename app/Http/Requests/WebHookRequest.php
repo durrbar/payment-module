3513f93e6eb1484a241d8c3d9416a17621d5c159
Kid Max <kidmax285@gmail.com>
2026-04-03 13:57:36 +0600

    fix(request): add minimal valid WebHookRequest class

---
+++ app/Http/Requests/WebHookRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Payment\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class WebHookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+}
